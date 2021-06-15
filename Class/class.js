e1c8236eaa7cc09021185eb98ed7a4db61b3be9b
DCoreas <josuepaiz487@gmail.com>
2021-06-15 11:10:44 -0600

    Add complete class

--- Class/class.js
+++ Class/class.js
@@ -16,3 +16,71 @@ console.log(task);
 console.log(task instanceof Task)
 
 //create class expressions
+const TaskList = class {
+  constructor() {
+    this.tasks = [
+      {title: 'Learn Firebase', completed: false},
+			{title: 'Learn CSS', completed: true},
+			{title: 'Learn HTML', completed: false},
+    ]
+  }
+  //getters -> obtienen valores, no aceptan parametros
+  //cuando la llamamos no ocupamos parentesis
+  get uncompleted() {
+    return [...this.tasks].filter(task => !task.completed);
+  }
+
+  get completed() {
+    return [...this.tasks].filter(task => task.completed);
+  }
+
+  //Setters -> no se invocan pero reciben parametros
+  set addTask(task) {
+    this.tasks.push(task)
+  }
+
+  //Methods
+  search(search) {
+    return this.tasks.find(task => task.title.includes(search));
+  }
+
+  removeTask(index) {
+    this.tasks.splice(index,1);
+  }
+
+  //static methods -> solo se pueden llamar desde la
+  //clase no se heredan, no se necesita instanciar la
+  // clase para utilizarlo
+  static normalizeTask(title) {
+    title = title.trim();
+    return title.charAt(0).toUpperCase() + title.substring(1)
+  }
+
+
+}
+
+const tareas = new TaskList();
+
+const tarea2 = new Task("Learn English");
+tareas.addTask = {...tarea2};
+
+console.log(tareas.uncompleted)
+console.log(tareas.search("English"));
+
+//sin necesidad de instanciar
+console.log(TaskList.normalizeTask("  hola que tal  "));
+
+//herencia o subclassing
+class Reminder extends Task {
+  constructor(title, date) {
+    super(title);
+    this.date = date;
+  }
+
+  get duration() {
+    return `${this.title} finaliza el ${this.date}`
+  }
+}
+
+const remiderTask = new Reminder("Learn React","30 de septimebre de 2021");
+console.log(remiderTask.duration)
